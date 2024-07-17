dd5c21d4a9bd1100a3c5d9b3ac726e6b4b4802ff
jawadshahid07 <jawad.shahid70@outlook.com>
2024-07-17 21:20:21 +0500

    initialized tailwind vite project

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,20 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#050505',
-        secondary: '#131c19',
-        tertiary: '#2a2b2d',
-      },
-      fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
