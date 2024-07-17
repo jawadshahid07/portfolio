7c1f26d91b1b9c5cbfb64afa9795ca8da110b118
jawadshahid07 <jawad.shahid70@outlook.com>
2024-07-17 22:20:25 +0500

    completed hero section

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,13 +1,15 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 export default function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-white-200 selection:text-black-200">
-      <div className="fixed top-0 -z-10 h-full w-full"></div>
-      <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="fixed top-0 z-[-10] h-full w-full"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse 80% 80% at 50% -20%, rgba(20, 20, 20, 0.8), rgba(0, 0, 0, 0.2))]"></div>
       <div className="container mx-auto px-8">
-        <Header/>
-        </div>
+        <Header />
+        <Hero/>
+      </div>
     </div>
-  )
+  );
 }
