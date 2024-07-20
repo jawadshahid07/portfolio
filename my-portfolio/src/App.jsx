3d16d18aa6e497f27d926f73dbf5483616bfe8f2
jawadshahid07 <jawad.shahid70@outlook.com>
2024-07-20 19:02:31 +0500

    completed projects section

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
+import Projects from "./components/Projects";
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
         <About/>
         <Technologies/>
         <Experience/>
+        <Projects/>
       </div>
     </div>
   );
