c06bcf76778619c644199820546a0c266d72ab2e
jawadshahid07 <jawad.shahid70@outlook.com>
2024-07-26 11:37:50 +0500

    updated cloudfront cache ttl

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -32,8 +32,8 @@ resource "aws_cloudfront_distribution" "cdn" {
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    default_ttl            = 450
+    max_ttl                = 10800
   }
 
   viewer_certificate {
